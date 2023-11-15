a1798dd1f2a59505c9204533a611156f352999e2
Sean Wang <seanwaterloo2997@gmail.com>
2023-11-15 01:15:21 -0500

    Update water_quality_sensor_code.ino

--- water_quality_sensor_code.ino
+++ water_quality_sensor_code.ino
@@ -1,9 +1,20 @@
+const int green_led = 12;
+const int red_led = 13;
+
 void setup() {
-  // put your setup code here, to run once:
-  
+  pinMode(green_led, OUTPUT);
+  pinMode(red_led, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // Clean Water
+  if (analogRead(A0) < 500) {
+    digitalWrite(green_led, HIGH);
+    digitalWrite(red_led, LOW);
+  }
+  // Dirty Water
+  else {
+    digitalWrite(green_led, LOW);
+    digitalWrite(red_led, HIGH);
+  }
 }
